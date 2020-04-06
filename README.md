2b1ec878801a0a8df11d5fa2fba19aa5bfdf946a
Devin <devingoulet@icloud.com>
2020-04-05 20:35:54 -0700

    deleted commented out code and updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 201-about-me
+
+# It just occured to me that I am supposed to document things in this file, my apologies. Enjoy my simple site.
--- app.js
+++ app.js
@@ -1,38 +1,5 @@
 'use strict'
 
-// arrays : useful for containing like things
-// var first = 'vanilla'
-// var second = 'mint-chip'
-// var third = 'moose tracks'
-
-// var userIceCream = prompt('ice cream?')
-
-// if(userIceCream === first || userIceCream === second || userIceCream === third){
-//     alert('we love the same icecream');
-// }
-
-// var icecreamILike = ['vanilla', 'mint-chip', 'moose tracks', 'chocolate chip cookie'];
-// icecreamILike[0]; 
-// aray access notation, it will be === 'vanilla'
-
-// if(icecreamILike[0] === userIceCream || icecreamILike[1] === userIceCream){
-//     alert('yay');
-// }
- 
-// do {
-//     var numbertheyPicked = ('number?');
-// } while (!numbertheyPicked);
-
-// var age = 0; // incrementor assignment
-// while(age < 60){// conditional
-    // alert('a decade has passed you are now ' + age);
-    // age = age + 1;
-    // age ++;// incrementation
-// }
-// i stands for INDEX, incrementor
-// for (var i = 0; < 6; i++){
-//     alert('a year has now passed you are now ' + i);
-// } 
 var Welcome;
 var livedinArizona;
 var workHistory;
@@ -125,41 +92,4 @@ do {
 guessCountB ++; 
 } while (guessCountB < 8);
         alert ('Nice, you managed to get ' + totalQuestionsRight + ' out of' + totalNumberofQuestions);
-
-// var userName = prompt('What is your name?');
-// alert('Thanks for stopping by ' + userName + '!')
-
-// var livedinArizona =  prompt('Have I ever lived in Arizona '
-// + userName + '?' + ' Y or N').toUpperCase();
-// if(livedinArizona === 'Y') {
-//     alert('Correct ' + userName + ' I did live there!');
-// } else {
-//     alert('Close, but no cigar ' + ' I did live there.');
-// }
-
-// var workHistory = prompt('Did I work at vans ' + userName + ' Y or N').toUpperCase();
-// if(workHistory === 'Y') {
-//     alert('Bingo, ' + 'my friend' + ' I used to sell shoes.')
-// } else {
-//     alert('Not this time kiddo, ' + ' come back later.')
-// }
-
-// var videoGames = prompt('Do I game ' + userName + ' Y or N').toUpperCase();
-// if(videoGames === 'Y') {
-//     alert('Once again you got it, ' + ' I do play videogames!')
-// } else {
-//     alert('Whatre you new? ' + userName + ' come back when you know me.')
-// }
-
-// var stayinWashington = prompt('Do I plan to stay in Washington ' + 'Y or N').toUpperCase();
-// if(stayinWashington === 'Y') {
-//     alert('How do you know so much about me? ' + userName + ' please, proceed.')
-// } else {
-//     alert('You think I like the rain? ' + userName + ' take a hike.')
-// }
-
-// var goRunning = prompt('Do I like to go for runs ' + 'Y or N').toUpperCase();
-// if(goRunning === 'Y') {
-//     alert('Ok, you passed ' + userName + ' now get outta here!')
-// } else {
-//     alert('I have had enough' + ' so long ' + userName);
+        
